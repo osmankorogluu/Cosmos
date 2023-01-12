325894fe1eb59779d6b8803cce42002ebf6c943b
Osman <korogluosman85@gmail.com>
2023-01-12 23:16:49 +0300

    Database context added

---
+++ src/Infrastructure/Cosmos.Persistence/Context/CosmosDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Cosmos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.Persistence.Context
+{
+    public class CosmosDatabaseContext: DbContext
+    {
+        public CosmosDatabaseContext()
+        {
+
+        }
+        public CosmosDatabaseContext(DbContextOptions<CosmosDatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
