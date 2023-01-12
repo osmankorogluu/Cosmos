84aa8ce84da032245964a139a338adda5a890233
Osman <korogluosman85@gmail.com>
2023-01-12 22:58:44 +0300

    Objects added

---
+++ src/Core/Cosmos.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
